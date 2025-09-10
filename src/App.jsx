8f8fc182fc6a1ae3dd2ca40606b9cb7493be1dd4
JacobTomaszewski22 <jake.tom@icloud.com>
2025-09-10 14:12:40 +0100

    Header done
    refactored header - Created a react element
    
    Refactored the soundcloud player - created a react element
    
    Moved the file structure around

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import BandImageHeader from "./BandImageHeader";
+import StickyPlayer from "./StickyPlayer";
+
+//Create our app component
+const App = () => {
+    return(
+        <div>
+            <BandImageHeader/>
+            <StickyPlayer />
+        </div>
+    );
+};
+
+//Create a container and set it to the root element
+const container = document.getElementById("root");
+const root = createRoot(container);
+//Render the app
+// root.render(<StrictMode><App/></StrictMode>);
+root.render(<App/>);
---
+++ src/BandImageHeader.jsx
@@ -0,0 +1,13 @@
+export default function BandImageHeader() {
+    return(
+        <div className = "header">
+            <img className = "header_image" 
+            src="/images/backgroundHeader.jpg"/>
+
+            <div className = "header_div">
+                <img className = "band_logo" 
+                src="/images/TextBC.png"/>
+            </div>
+        </div>
+    )
+}
