69a373e14a5b41971053038107ef3b3e59db8db1
JacobTomaszewski22 <jake.tom@icloud.com>
2025-09-13 17:27:31 +0100

    minor change

--- src/EmailerSignup.jsx
+++ src/EmailerSignup.jsx
@@ -1,6 +1,7 @@
 if (
   process.env.NODE_ENV === "development" ||
-  process.env.NODE_ENV === "testing"
+  process.env.NODE_ENV === "testing" ||
+  process.env.NODE_ENV === "staging"
 ) {
   localStorage.removeItem("enteredEmail");
 }
