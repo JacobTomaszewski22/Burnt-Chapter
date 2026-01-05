9494b55f1caae8198eaee2ed2236e987a60cfcde
JacobTomaszewski22 <jake.tom@icloud.com>
2026-01-05 15:00:17 +0000

    Scorched Live

--- src/index/NewMusic.jsx
+++ src/index/NewMusic.jsx
@@ -8,30 +8,20 @@ export default function NewMusic() {
         <div className="mew-music-image-container">
           <img
             className="new-music-image"
-            src="/images/bleed.webp"
+            src="/images/scorched.webp"
             loading="lazy"
           />
         </div>
         <div className="new-music-writeup">
-          <h3 className="new-music-song-title">Bleed For Me</h3>
+          <h3 className="new-music-song-title">Scorched</h3>
           <p>
-            We are releasing our fourth single ‘Bleed For Me’ on October 31st! A
-            massive song with a catchy chorus that WILL get stuck in your head.
+            We have now released our first EP: Scorched
           </p>
           <p>
-            This song is about innocent people being sent off to die in a war
-            they don’t care for. Its themes involve Death, Destruction and
-            Inevitability. Musically it is a song you can sing to and you
-            Heathens that have seen us like know: The crowd loves it!.
+            This EP is the culmination of our first year of writing, capturing the essence of Burnt Chapter. It contains four of our released tracks, and one previously unreleased track. This is now a complete artistic product and how we always intended for our listeners to hear the tracks. We hope you enjoy listening to it as much as we enjoyed creating it.
           </p>
-          <p>Pre save link:</p>
           <div className="new-music-presave-link-container">
-            <a
-              href="https://distrokid.com/hyperfollow/burntchapter/bleed-for-me/"
-              className="new-music-presave-link"
-            >
-              https://distrokid.com/hyperfollow/burntchapter/bleed-for-me/
-            </a>
+          <iframe data-testid="embed-iframe" src="https://open.spotify.com/embed/album/3xaomAWtR3PqRum0aIIBmr?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
           </div>
         </div>
       </div>
